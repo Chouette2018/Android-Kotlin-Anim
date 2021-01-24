223119aa975f856bd19119d3947f6df1d404db5c
Chouette2018 <40530368+Chouette2018@users.noreply.github.com>
2021-01-24 10:42:20 -0800

    Added Progress circle Added custom views attributes

--- projects/nd940-c3-advanced-android-programming-project-starter-master/starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ projects/nd940-c3-advanced-android-programming-project-starter-master/starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,14 +1,18 @@
 package com.udacity
 
-import android.animation.*
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import android.view.animation.Animation
-import androidx.core.content.ContextCompat.getColor
-import androidx.core.content.res.ResourcesCompat
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -19,29 +23,16 @@ class LoadingButton @JvmOverloads constructor(
 
     private var progress = 0.0f
 
-    private var paintDefault = Paint().apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        typeface = Typeface.create("", Typeface.BOLD)
-        color = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
-    }
+    private var textDefaultColor = 0
+    private var textActiveColor = 0
+    private var backgroundActiveColor = 0
+    private var backgroundDefaultColor = 0
 
-    private var paintInProgress = Paint().apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        typeface = Typeface.create("", Typeface.BOLD)
-        color = ResourcesCompat.getColor(resources, R.color.colorPrimaryDark, null)
-    }
+    private var paintDefault: Paint
 
-    private var paintText = Paint().apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        typeface = Typeface.create("", Typeface.BOLD)
-        color = ResourcesCompat.getColor(resources, R.color.white, null)
-    }
+    private var paintInProgress: Paint
+
+    private var paintText: Paint
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
         Log.e("gds state", new.text)
@@ -51,6 +42,29 @@ class LoadingButton @JvmOverloads constructor(
 
     init {
         isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            textDefaultColor = getColor(R.styleable.LoadingButton_textDefaultColor, 0)
+            textActiveColor = getColor(R.styleable.LoadingButton_textActiveColor, 0)
+            backgroundDefaultColor = getColor(R.styleable.LoadingButton_backgroundDefaultColor, 0)
+            backgroundActiveColor = getColor(R.styleable.LoadingButton_backgroundActiveColor, 0)
+        }
+
+        paintDefault = Paint().apply {
+            style = Paint.Style.FILL
+            textAlign = Paint.Align.CENTER
+            textSize = 55.0f
+            typeface = Typeface.create("", Typeface.BOLD)
+            color = backgroundDefaultColor
+        }
+
+        paintInProgress = Paint(paintDefault).apply {
+            color = backgroundActiveColor
+        }
+
+        paintText = Paint(paintDefault).apply {
+            color = textDefaultColor
+        }
     }
 
     fun setProgress(newValue: Float) {
@@ -61,7 +75,7 @@ class LoadingButton @JvmOverloads constructor(
                 buttonState = ButtonState.Completed
                 progress = 0f
                 isClickable = true
-            }else {
+            } else {
                 invalidate()
             }
         }
@@ -69,15 +83,31 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        //drawing background progress bar
+        paintText.color =
+            if (buttonState.downloadInProgress == DOWNLOAD_IN_PROGRESS) textActiveColor else textDefaultColor
         synchronized(this) {
             val progressWidth: Int = (widthSize * progress).toInt()
             canvas.drawRect(Rect(0, 0, progressWidth, heightSize), paintInProgress)
             canvas.drawRect(Rect(progressWidth, 0, widthSize, heightSize), paintDefault)
         }
 
+        //drawing title
         val xPos = (canvas.getWidth() / 2.0).toFloat()
         val yPos = ((canvas.getHeight() / 2) - ((paintText.descent() + paintText.ascent()) / 2))
         canvas.drawText(buttonState.text, xPos, yPos, paintText)
+
+        //Drawing progress circle
+        val width = paintText.measureText(buttonState.text)
+        val radius = heightSize * 0.25f
+        val progressInDegrees = 360 * progress
+        //defining an oval of 2 radius by 2 radius
+        val left = xPos + width / 2 + radius
+        val right = left + 2 * radius
+        val top = radius
+        val bottom = top + 2 * radius
+        canvas.drawArc(left, top, right, bottom, 0f, progressInDegrees, true, paintText)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
